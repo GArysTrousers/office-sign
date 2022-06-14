c57d970c11c9e198db1a65454279f8b39150a388
Ben Lee <bjonmere@gmail.com>
2022-06-14 15:05:20 +1000

    [add] static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -10,8 +10,19 @@ const config = {
   }),
 
 	kit: {
-		adapter: adapter()
-	}
+		adapter: adapter({
+      // default options are shown
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+      precompress: false
+    })
+	},
+
+  prerender: {
+    // This can be false if you're using a fallback (i.e. SPA mode)
+    default: true
+  }
 };
 
 export default config;
