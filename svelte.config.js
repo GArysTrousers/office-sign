732df2c25a18ff181202e65778fcd9d9c751700a
Ben Lee <bjonmere@gmail.com>
2022-06-15 08:35:40 +1000

    [add] node export

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,19 +10,8 @@ const config = {
   }),
 
 	kit: {
-		adapter: adapter({
-      // default options are shown
-      pages: 'build',
-      assets: 'build',
-      fallback: null,
-      precompress: false
-    })
+		adapter: adapter()
 	},
-
-  prerender: {
-    // This can be false if you're using a fallback (i.e. SPA mode)
-    default: true
-  }
 };
 
 export default config;
