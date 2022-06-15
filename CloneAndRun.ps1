2ca4dc097676b163fed5c92e69e9bd11dc30e54d
Ben Lee <bjonmere@gmail.com>
2022-06-15 12:40:02 +1000

    [add] event functions, clone and run script

---
+++ CloneAndRun.ps1
@@ -0,0 +1,5 @@
+cd ..
+git clone https://github.com/GArysTrousers/office-sign.git ./office-sign
+cd ./office-sign
+npm run build
+node ./build
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,60 +1,113 @@
 <script lang="ts">
-import { onMount } from "svelte";
-
+  import { onMount } from "svelte";
 
   let messages = [
     {
-      title: "IT is Open",
+      title: "ICT is Closed for the day",
       body: "",
-      bg: "bg-green-700",
+      bg: "bg-black",
+      onStart: () => {},
+      onTimeout: () => {},
+      onEnd: () => {},
     },
     {
-      title: "IT is Closed",
+      title: "ICT is Open",
+      body: "",
+      bg: "bg-green-700",
+      onStart: () => {},
+      onTimeout: () => {},
+      onEnd: () => {},
+    },
+    {
+      title: "ICT is Closed",
       body: "Check back later",
       bg: "bg-red-700",
+      onStart: () => {},
+      onTimeout: () => {},
+      onEnd: () => {},
+    },
+    {
+      title: "ICT is Closed",
+      body: "Check back in:",
+      bg: "bg-red-700",
+      onStart: () => {addTime(3)},
+      onTimeout: () => {changeMessage(4)},
+      onEnd: () => {},
+    },
+    {
+      title: "ICT will be back shortly",
+      body: "",
+      bg: "bg-yellow-600",
+      onStart: () => {},
+      onTimeout: () => {},
+      onEnd: () => {},
     },
   ];
-  let selected = messages[0];
+  let selected = messages[1];
   let timer: any;
-  let timeLeft:number = 0;
+  let timeLeft: number = 0;
 
-  onMount(async () => {
+  onMount(async () => {});
 
-  });
+  function changeMessage(index:number) {
+    stopTimer()
+    selected.onEnd();
+    selected = messages[index];
+    selected.onStart();
+  }
 
-  function addTime(length:number) {
-    clearInterval(timer)
-    timeLeft += length
+  function addTime(length: number) {
+    clearInterval(timer);
+    timeLeft += length;
     timer = setInterval(() => {
-      timeLeft -= 1
+      timeLeft -= 1;
       if (timeLeft <= 0) {
-        clearInterval(timer)
+        clearInterval(timer);
+        selected.onTimeout();
       }
-    }, 1000)
+    }, 1000);
   }
 
   function stopTimer() {
-    clearInterval(timer)
-    timeLeft = 0
+    clearInterval(timer);
+    timeLeft = 0;
   }
 
-  function convertToTime(time:number) {
-    return `${Math.floor(time/60)}:${String(time % 60).padStart(2, '0')}`
+  function convertToTime(time: number) {
+    return `${Math.floor(time / 60)}:${String(time % 60).padStart(2, "0")}`;
+  }
+
+  function onKeyDown(e: KeyboardEvent) {
+    let num = e.keyCode - 48;
+
+    if (num >= 0 && num <= 9) {
+      if (num < messages.length) {
+        changeMessage(num);
+      }
+      return;
+    }
+
+    if (e.key == " ") addTime(600);
+    else if (e.key == "Backspace") stopTimer();
   }
 </script>
 
-<div class="flex flex-row gap-5">
-  <button on:click={() => selected = messages[0]}>Open</button>
-  <button on:click={() => selected = messages[1]}>Close</button>
+<svelte:window on:keydown={onKeyDown} />
+
+<div class="fixed flex flex-row gap-5 hidden">
+  <button on:click={() => (selected = messages[0])}>Open</button>
+  <button on:click={() => (selected = messages[1])}>Close</button>
   <button on:click={() => addTime(600)}>+Time</button>
   <button on:click={() => stopTimer()}>ClearTime</button>
 </div>
-<main class="{selected.bg} text-white flex flex-col justify-center items-center text-center">
+<main
+  class="{selected.bg} text-white flex flex-col justify-center items-center text-center"
+>
   <div class="flex-col justify-center items-center">
-    <h1 class="text-5xl mb-6">{selected.title}</h1>
-    <div class="text-2xl">{selected.body}</div>
+    <h1 class="text-9xl mb-6">{selected.title}</h1>
+    <div class="text-4xl">{selected.body}</div>
     {#if timeLeft > 0}
-      <div class="text-7xl">{convertToTime(timeLeft)}</div>
+      <div class="text-9xl">{convertToTime(timeLeft)}</div>
     {/if}
   </div>
 </main>
