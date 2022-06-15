f1f99f77c9e1c1cf2aa23433452bf2d86a1f8d56
Ben Lee <bjonmere@gmail.com>
2022-06-16 08:56:58 +1000

    [add] kiosk mode

--- StartBrowser.ps1
+++ StartBrowser.ps1
@@ -1,2 +1,11 @@
+$edge = "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+$url = "http://localhost:3000"
+
 Start-Sleep 3
-Start http://localhost:3000
+
+if (Test-Path $edge) {
+  Start-Process $edge -ArgumentList  "--kiosk", $url, "--edge-kiosk-type=fullscreen"
+}
+else {
+  Start-Process $url
+}
